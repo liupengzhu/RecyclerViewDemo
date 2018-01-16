17c199aa20da9f35d35e830642bd32ad59660171
朱留鹏 <>
2018-01-16 15:00:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sddt/recyclerviewdemo/Fruit.java
@@ -0,0 +1,21 @@
+package com.example.sddt.recyclerviewdemo;
+
+/**
+ * Created by sddt on 18-1-16.
+ */
+
+public class Fruit {
+    private String fruitName;
+
+    public Fruit(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public void setFruitName(String fruitName) {
+        this.fruitName = fruitName;
+    }
+}
